ee0168d6c5487ce2972865db96d47f100fabc6e4
PaudelSiddhanta <sp7007@nyu.edu>
2025-12-10 20:41:43 -0500

    changed flight and hotel finder agent

--- backend/agents/phase2/flight_finder/agent.py
+++ backend/agents/phase2/flight_finder/agent.py
@@ -4,60 +4,37 @@ from typing import List, Dict, Any
 from google.adk.agents import Agent
 from google.adk.tools import FunctionTool
 from google.adk.tools.agent_tool import AgentTool
-from .flight_utils import (
+from .flight_hotel_utils import (
     find_airports,
     fallback_scraper,
-    get_all_flights
+    scrape_hotels,
 )
 from agents.utils.instructions_loader import load_instruction_from_file
-# Agent instructions
-flight_finder_instructions = load_instruction_from_file(
-    "flight_finder.txt")
 
 bestflight_agent = Agent(
     name="bestflight_agent",
-    model="gemini-2.0-flash",
-    instruction=""" Given all the flight options, pick the TOP 2 best flights considering:
-      1. Price (lower is better)
-      2. Duration (shorter is better)  
-      3. Number of stops (fewer is better)
-      Return only 2 flights in the following format. Remember that the following are just examples.
-      You must analyze the flight data available to you and pick the best 2 flights. The example below is just for reference.
-      [
-      {departure_airport: 'John F. Kennedy International Airport(JFK)' 
-      departure_time:'2020-01-06 23:20' 
-      arrival_airport: 'Hamad International Airport(DOH)' 
-      arrival_time: '2020-01-07 06:40'
-      duration: '740'
-      airplane: 'Boeing 777'
-      airline: 'Qatar Airways',
-      price: '$2970'},
-      {departure_airport:'John F. Kennedy International Airport(JFK)' 
-      departure_time:'2020-01-06 15:30' 
-      arrival_airport: 'Hamad International Airport(DOH)' 
-      arrival_time: '2020-01-07 02:10'
-      duration: '800'
-      airplane: 'Airbus A350'
-      airline: 'Qatar Airways',
-      price: '$2508'}
-      ]
-
-   """,
+    model="gemini-2.5-pro",
+    instruction=load_instruction_from_file(
+        "best_flight_agent_instructions.txt"
+    ),
     description="Finds the best roundtrip flights between cities using airport lookup, flight search, and intelligent selection.",
 )
 
 
-
+flight_hotel_finder_instructions = load_instruction_from_file(
+    "flight_hotel_finder_agent_instructions.txt"
+)
 
 # Create the Flight Finder Agent
-flight_agent = Agent(
-    name="flight_agent",
-    model="gemini-2.0-flash",
-    instruction=flight_finder_instructions,
-    description="Finds the best roundtrip flights between cities using airport lookup, flight search, and intelligent selection.",
+flight_hotel_finder_agent = Agent(
+    name="flight_hotel_finder_agent",
+    model="gemini-2.5-pro",
+    instruction=flight_hotel_finder_instructions,
+    description="Finds the best roundtrip flights between cities using airport lookup, flight search, and intelligent selection and also provides appropriate hotel choices for the duration of stay.",
     tools=[
         FunctionTool(find_airports),
         FunctionTool(fallback_scraper),
+        FunctionTool(scrape_hotels),
         AgentTool(bestflight_agent),
     ]
 )
